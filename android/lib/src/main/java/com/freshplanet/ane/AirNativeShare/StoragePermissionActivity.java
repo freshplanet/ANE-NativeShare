e88d16b5f6b35852995954ececc6321a02a0bfad
Mateo Kozomara <mateo.kozomara@gmail.com>
2020-10-22 21:35:46 +0200

    use android api 29

--- android/lib/src/main/java/com/freshplanet/ane/AirNativeShare/StoragePermissionActivity.java
+++ android/lib/src/main/java/com/freshplanet/ane/AirNativeShare/StoragePermissionActivity.java
@@ -4,10 +4,11 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class StoragePermissionActivity extends Activity {
 
